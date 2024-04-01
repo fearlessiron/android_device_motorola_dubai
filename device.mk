294ffb6f7dc7173a2eaace6b1a15a0c28ad34ca3
Florian Esser <android-dev@fesr.de>
2024-04-01 13:36:57 +0200

    dubai: add microG GmsCore package

--- device.mk
+++ device.mk
@@ -103,5 +103,9 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-dubai-game-perf.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-dubai.conf
 
+# MicroG
+PRODUCT_PACKAGES += \
+    GmsCore
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/dubai/dubai-vendor.mk)
